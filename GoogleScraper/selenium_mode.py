85929d4ccbc074b8752271814f92ec44ffc04836
Rafal Lagowski <lagowski@pro4.es>
2016-12-20 19:18:52 +0100

    correct

--- GoogleScraper/selenium_mode.py
+++ GoogleScraper/selenium_mode.py
@@ -505,7 +505,7 @@ class SelScrape(SearchEngineScrape, threading.Thread):
                         until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, selector), str(self.page_number)))
                 except TimeoutException as e:
                     self._save_debug_screenshot()
-                    self.webdriver.get_screenshot_as_png("out.png")
+                    self.webdriver.get_screenshot_as_file("./out.png")
                     try:
                         content = self.webdriver.find_element_by_css_selector(selector).text
                         # raise Exception('Pagenumber={} did not appear in navigation. Got "{}" instead'\
