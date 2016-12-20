c3a494d8d70417a09c3bf54bf3abb853d21d6e0a
Rafal Lagowski <lagowski@pro4.es>
2016-12-20 18:50:52 +0100

    test

--- GoogleScraper/selenium_mode.py
+++ GoogleScraper/selenium_mode.py
@@ -502,12 +502,13 @@ class SelScrape(SearchEngineScrape, threading.Thread):
 
                 try:
                     WebDriverWait(self.webdriver, 5).\
-                        until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, selector), str(self.page_number-1)))
+                        until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, selector), str(self.page_number)))
                 except TimeoutException as e:
                     self._save_debug_screenshot()
                     content = self.webdriver.find_element_by_css_selector(selector).text
                     # raise Exception('Pagenumber={} did not appear in navigation. Got "{}" instead'\
                     #                 .format(self.page_number, content))
+                    print ("lkjlkj", e)
                     print('!!!!SKIPPING!!!! Pagenumber={} did not appear in navigation. Got "{}" instead'.format(self.page_number, content))
 
         elif self.search_type == 'image':
@@ -636,7 +637,7 @@ class SelScrape(SearchEngineScrape, threading.Thread):
             logger.debug('Cannot set window size: {}'.format(e))
 
         super().before_search()
-
+        print("***********************************", self.webdriver)
         if self.startable:
             self.build_search()
             self.search()
