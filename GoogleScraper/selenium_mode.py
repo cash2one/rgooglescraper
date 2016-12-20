edf57b256866f28717221a8cdea9e257204bff85
Rafal Lagowski <lagowski@pro4.es>
2016-12-20 19:35:16 +0100

    Write screens

--- GoogleScraper/selenium_mode.py
+++ GoogleScraper/selenium_mode.py
@@ -463,7 +463,18 @@ class SelScrape(SearchEngineScrape, threading.Thread):
                 self._save_debug_screenshot()
                 # raise Exception('{}: Cannot locate next page element: {}'.format(self.name, str(e)))
                 print('!!!!SKIPPING!!!!  {}: Cannot locate next page element: {}'.format(self.name, str(e)))
-
+                try:
+                    content = self.webdriver.find_element_by_css_selector(selector).text
+                    # raise Exception('Pagenumber={} did not appear in navigation. Got "{}" instead'\
+                    #                 .format(self.page_number, content))
+                    print('*** ERROR: {}: Cannot locate next page element: {}'.format(self.name, str(e)))
+                    self.webdriver.get_screenshot_as_file(
+                        "./can_not_locate_element-"+self.name+"-"+datetime.now().strftime("%Y%m%d-%H%M%S") + ".png")
+                    raise Exception('Pages number confusion')
+                except:
+                    self.webdriver.get_screenshot_as_file(
+                        "./captcha-" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".png")
+                    print("*** PROBABLY CATCHA")
             return self.webdriver.find_element_by_css_selector(selector)
 
         elif self.search_type == 'image':
@@ -505,13 +516,16 @@ class SelScrape(SearchEngineScrape, threading.Thread):
                         until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, selector), str(self.page_number)))
                 except TimeoutException as e:
                     self._save_debug_screenshot()
-                    self.webdriver.get_screenshot_as_file("./out.png")
                     try:
                         content = self.webdriver.find_element_by_css_selector(selector).text
                         # raise Exception('Pagenumber={} did not appear in navigation. Got "{}" instead'\
                         #                 .format(self.page_number, content))
                         print('*** ERROR: Pagenumber={} did not appear in navigation. Got "{}" instead'.format(self.page_number, content))
+                        self.webdriver.get_screenshot_as_file(
+                            "./pagenumber-"+self.page_number+"-"+content+"-"+datetime.now().strftime("%Y%m%d-%H%M%S") + ".png")
+                        raise Exception('Pages number confusion')
                     except:
+                        self.webdriver.get_screenshot_as_file("./captcha-"+datetime.now().strftime("%Y%m%d-%H%M%S")+".png")
                         print("*** PROBABLY CATCHA")
         elif self.search_type == 'image':
             self.wait_until_title_contains_keyword()
